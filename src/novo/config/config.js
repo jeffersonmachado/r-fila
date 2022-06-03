a5f9245f3c1d735327a3d375168e9e5677d6f3c2
Thiago <thiagogalvao2001m@gmail.com>
2022-06-03 12:36:07 -0300

    'atualizar'

--- src/novo/config/config.js
+++ src/novo/config/config.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import configData from "../../config.json";
 import Modal from 'react-modal';
+import css from './config.css';
 
 
 function Config(){
@@ -31,7 +32,7 @@ function Config(){
                         <h3 className='text-center'>Editar Dados</h3>
                         <form>
                             <label>Novo Tamanho: </label>
-                            <input type='number' className='form-control' defaultValue={Object.values(configData.botao_filas)} />
+                            <input type='number' className='form-control border border-danger' defaultValue={Object.values(configData.botao_filas)} />
                             <button className='btn btn-success mt-3'>Enviar</button>
                         </form>
                         <button onClick={() => setModal1(false) } className='btn btn-danger form-control mt-4'>Voltar</button>
@@ -60,39 +61,204 @@ function Config(){
                         <button onClick={() => setModal2(true) } className='btn btn-success'>Editar</button>
                         <Modal isOpen={!!props2} onRequestClose={props2.clearSelectedOption} ariaHideApp={false} contentLabel="props2">
                             <h3 className='text-center'>Editar Dados</h3>
-                            <form>
+                            <form>                                
                                 <label>Novo {Object.values(configData.atendimento_colunas.id)}: </label>
-                                <input type='text' className='form-control' />
+                                <input type='text' className='border border-danger form-control'/>
 
-                                <label className='mt-1'>Novo {Object.values(configData.atendimento_colunas.datatime)}: </label>
-                                <input type='text' className='form-control' />
+                                <div className=''>
+                                    <div className='form-check mt-1 mb-1 escolhas'>
+                                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck1" />
+                                        <label className="form-check-label" for="defaultCheck1">
+                                            True: {Object.values(configData.atendimento_colunas.id)}
+                                        </label>
+                                    </div>
+                                    <div className="form-check mt-1 mb-1 mx-2 escolhas">
+                                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck2" />
+                                        <label className="form-check-label" for="defaultCheck2">
+                                            False: {Object.values(configData.atendimento_colunas.id)}
+                                        </label>
+                                    </div>
+                                </div>    
+                                
+                                <label className='mt-2'>Novo {Object.values(configData.atendimento_colunas.datatime)}: </label>
+                                <input type='text' className='form-control border border-danger' />
 
-                                <label className='mt-1'>Novo {Object.values(configData.atendimento_colunas.id_das_filas)}: </label>
-                                <input type='text' className='form-control' />
+                                <div className=''>
+                                    <div className='form-check mt-1 mb-1 escolhas'>
+                                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck3" />
+                                        <label className="form-check-label" for="defaultCheck3">
+                                            True: {Object.values(configData.atendimento_colunas.datatime)}
+                                        </label>
+                                    </div>
+                                    <div className="form-check mt-1 mb-1 mx-2 escolhas">
+                                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck4" />
+                                        <label className="form-check-label" for="defaultCheck4">
+                                            False: {Object.values(configData.atendimento_colunas.datatime)}
+                                        </label>
+                                    </div>
+                                </div> 
 
-                                <label className='mt-1'>Novo {Object.values(configData.atendimento_colunas.atendente)}: </label>
-                                <input type='text' className='form-control' />
+                                <label className='mt-2'>Novo {Object.values(configData.atendimento_colunas.id_das_filas)}: </label>
+                                <input type='text' className='form-control border border-danger' />
 
-                                <label className='mt-1'>Novo {Object.values(configData.atendimento_colunas.datainicio)}: </label>
-                                <input type='text' className='form-control' />
+                                <div className=''>
+                                    <div className='form-check mt-1 mb-1 escolhas'>
+                                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck5" />
+                                        <label className="form-check-label" for="defaultCheck5">
+                                            True: {Object.values(configData.atendimento_colunas.id_das_filas)}
+                                        </label>
+                                    </div>
+                                    <div className="form-check mt-1 mb-1 mx-2 escolhas">
+                                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck6" />
+                                        <label className="form-check-label" for="defaultCheck6">
+                                            False: {Object.values(configData.atendimento_colunas.id_das_filas)}
+                                        </label>
+                                    </div>
+                                </div>
+    
+                                <label className='mt-2'>Novo {Object.values(configData.atendimento_colunas.atendente)}: </label>
+                                <input type='text' className='form-control border border-danger' />
 
-                                <label className='mt-1'>Novo {Object.values(configData.atendimento_colunas.datafinal)}: </label>
-                                <input type='text' className='form-control' />
+                                <div className=''>
+                                    <div className='form-check mt-1 mb-1 escolhas'>
+                                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck7" />
+                                        <label className="form-check-label" for="defaultCheck7">
+                                            True: {Object.values(configData.atendimento_colunas.atendente)}
+                                        </label>
+                                    </div>
+                                    <div className="form-check mt-1 mb-1 mx-2 escolhas">
+                                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck8" />
+                                        <label className="form-check-label" for="defaultCheck8">
+                                            False: {Object.values(configData.atendimento_colunas.atendente)}
+                                        </label>
+                                    </div>
+                                </div>
 
-                                <label className='mt-1'>Novo {Object.values(configData.atendimento_colunas.status1)}: </label>
-                                <input type='text' className='form-control' />
+                                <label className='mt-2'>Novo {Object.values(configData.atendimento_colunas.datainicio)}: </label>
+                                <input type='text' className='form-control border border-danger' />
 
-                                <label className='mt-1'>Novo {Object.values(configData.atendimento_colunas.contador)}: </label>
-                                <input type='text' className='form-control' />
+                                <div className=''>
+                                    <div className='form-check mt-1 mb-1 escolhas'>
+                                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck9" />
+                                        <label className="form-check-label" for="defaultCheck9">
+                                            True: {Object.values(configData.atendimento_colunas.datainicio)}
+                                        </label>
+                                    </div>
+                                    <div className="form-check mt-1 mb-1 mx-2 escolhas">
+                                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck10" />
+                                        <label className="form-check-label" for="defaultCheck10">
+                                            False: {Object.values(configData.atendimento_colunas.datainicio)}
+                                        </label>
+                                    </div>
+                                </div>
 
-                                <label className='mt-1'>Novo {Object.values(configData.atendimento_colunas.Guichê)}: </label>
-                                <input type='text' className='form-control' />
+                                <label className='mt-2'>Novo {Object.values(configData.atendimento_colunas.datafinal)}: </label>
+                                <input type='text' className='form-control border border-danger' />
 
-                                <label className='mt-1'>Novo {Object.values(configData.atendimento_colunas.nome_fila)}: </label>
-                                <input type='text' className='form-control' />
+                                <div className=''>
+                                    <div className='form-check mt-1 mb-1 escolhas'>
+                                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck11" />
+                                        <label className="form-check-label" for="defaultCheck11">
+                                            True: {Object.values(configData.atendimento_colunas.datafinal)}
+                                        </label>
+                                    </div>
+                                    <div className="form-check mt-1 mb-1 mx-2 escolhas">
+                                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck12" />
+                                        <label className="form-check-label" for="defaultCheck12">
+                                            False: {Object.values(configData.atendimento_colunas.datafinal)}
+                                        </label>
+                                    </div>
+                                </div>
 
-                                <label className='mt-1'>Novo {Object.values(configData.atendimento_colunas.alerta)}: </label>
-                                <input type='text' className='form-control' />
+                                <label className='mt-2'>Novo {Object.values(configData.atendimento_colunas.status1)}: </label>
+                                <input type='text' className='form-control border border-danger' />
+
+                                <div className=''>
+                                    <div className='form-check mt-1 mb-1 escolhas'>
+                                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck13" />
+                                        <label className="form-check-label" for="defaultCheck13">
+                                            True: {Object.values(configData.atendimento_colunas.status1)}
+                                        </label>
+                                    </div>
+                                    <div className="form-check mt-1 mb-1 mx-2 escolhas">
+                                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck14" />
+                                        <label className="form-check-label" for="defaultCheck14">
+                                            False: {Object.values(configData.atendimento_colunas.status1)}
+                                        </label>
+                                    </div>
+                                </div>
+
+                                <label className='mt-2'>Novo {Object.values(configData.atendimento_colunas.contador)}: </label>
+                                <input type='text' className='form-control border border-danger' />
+
+                                <div className=''>
+                                    <div className='form-check mt-1 mb-1 escolhas'>
+                                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck15" />
+                                        <label className="form-check-label" for="defaultCheck15">
+                                            True: {Object.values(configData.atendimento_colunas.contador)}
+                                        </label>
+                                    </div>
+                                    <div className="form-check mt-1 mb-1 mx-2 escolhas">
+                                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck16" />
+                                        <label className="form-check-label" for="defaultCheck16">
+                                            False: {Object.values(configData.atendimento_colunas.contador)}
+                                        </label>
+                                    </div>
+                                </div>
+
+                                <label className='mt-2'>Novo {Object.values(configData.atendimento_colunas.Guichê)}: </label>
+                                <input type='text' className='form-control border border-danger' />
+
+                                <div className=''>
+                                    <div className='form-check mt-1 mb-1 escolhas'>
+                                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck17" />
+                                        <label className="form-check-label" for="defaultCheck17">
+                                            True: {Object.values(configData.atendimento_colunas.Guichê)}
+                                        </label>
+                                    </div>
+                                    <div className="form-check mt-1 mb-1 mx-2 escolhas">
+                                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck18" />
+                                        <label className="form-check-label" for="defaultCheck18">
+                                            False: {Object.values(configData.atendimento_colunas.Guichê)}
+                                        </label>
+                                    </div>
+                                </div>
+
+                                <label className='mt-2'>Novo {Object.values(configData.atendimento_colunas.nome_fila)}: </label>
+                                <input type='text' className='form-control border border-danger' />
+
+                                <div className=''>
+                                    <div className='form-check mt-1 mb-1 escolhas'>
+                                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck19" />
+                                        <label className="form-check-label" for="defaultCheck19">
+                                            True: {Object.values(configData.atendimento_colunas.nome_fila)}
+                                        </label>
+                                    </div>
+                                    <div className="form-check mt-1 mb-1 mx-2 escolhas">
+                                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck20" />
+                                        <label className="form-check-label" for="defaultCheck20">
+                                            False: {Object.values(configData.atendimento_colunas.nome_fila)}
+                                        </label>
+                                    </div>
+                                </div>
+
+                                <label className='mt-2'>Novo {Object.values(configData.atendimento_colunas.alerta)}: </label>
+                                <input type='text' className='form-control border border-danger' />
+
+                                <div className=''>
+                                    <div className='form-check mt-1 mb-1 escolhas'>
+                                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck21" />
+                                        <label className="form-check-label" for="defaultCheck21">
+                                            True: {Object.values(configData.atendimento_colunas.alerta)}
+                                        </label>
+                                    </div>
+                                    <div className="form-check mt-1 mb-1 mx-2 escolhas">
+                                        <input className="form-check-input" type="checkbox" value="" id="defaultCheck22" />
+                                        <label className="form-check-label" for="defaultCheck22">
+                                            False: {Object.values(configData.atendimento_colunas.alerta)}
+                                        </label>
+                                    </div>
+                                </div>
 
                                 <button className='btn btn-success mt-3'>Enviar</button>
 
